b87aaef6e0fb466b89779bfedfcccc8732f91cfa
Happy_Virus / IkBeom <66310877+Happy-Virus-IkBeom@users.noreply.github.com>
2020-08-12 19:16:22 +0900

    Update reinitialize_argfile.py

--- mnist_fc/argfiles/reinitialize_argfile.py
+++ mnist_fc/argfiles/reinitialize_argfile.py
@@ -23,12 +23,12 @@ from absl import flags
 from lottery_ticket.foundations import paths
 from lottery_ticket.mnist_fc import constants
 
-FLAGS = flags.FLAGS
-
+FLAGS = flags.FLAGS # ex) flags 객체는 int, float, boolean, string 의 값을 저장하고, 가져다 사용하기 쉽게 해주는 기능을 합니다.
+                    # FLAGS.learning_rate 을 입력하면 0.01 출력
 flags.DEFINE_enum('experiment', 'reinit', ['reinit', 'reuse', 'reuse_sign'],
                   'Which experiment to run.')
-
-
+                    # 이 객체는 기본적으로 제공되는 DEFINE_*** 로 시작되는 함수를 통해서 key, value 형식과 비슷하게 원하는 데이터를 정의할 수 있게 됩니다.
+                    # 이들 함수의 세번째 arg는 도큐먼트를 위한 스트링 값임으로 신경쓰지 않아도 됩니다.
 def main(argv):
   del argv  # Unused.
   line_format = ('--masks={masks} --output_dir={output_dir}')
